05b4893e2aaf24a91a7f3dd954176ecf35360386
Allir <dawidus250@gmail.com>
2024-01-24 21:54:35 +0100

    done 1, grade 4

--- projekt1/main.cpp
+++ projekt1/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include "Logger.h"
 
+
+struct memory{
+    int a;
+};
+
+void DumpFunction(FILE*f,void* args){
+    auto arg=(memory*) args;
+    fwrite(arg,sizeof(memory),1,f);
+}
+
+
+
 int main() {
+    memory mem{
+        .a=5
+    };
+    Logger::SetMemoryDumpFunction(DumpFunction,(void*)&mem);
     std::cout<<::getpid()<<std::endl;
     for(int i=0;i<1000;i++){
         sleep(1);
---
+++ projekt2/Scheduler.h
@@ -0,0 +1,10 @@
+#ifndef PROJEKT2_SCHEDULER_H
+#define PROJEKT2_SCHEDULER_H
+
+
+class Scheduler {
+
+};
+
+
+#endif //PROJEKT2_SCHEDULER_H
